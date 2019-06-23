2a0c834d7ed6fc141914dd1c026edb6c18a9c8a5
David Roundy <roundyd@physics.oregonstate.edu>
2019-06-22 20:19:18 -0700

    add concreteness data

--- memorable-wordlist.py
+++ memorable-wordlist.py
@@ -24,6 +24,16 @@ with open('aoa.txt') as f:
         if fields[3] != '#N/A' and len(fields[3]) > 0:
             aoa_rating[fields[0]] = float(fields[3])
 
+concreteness = {}
+percent_known = {}
+with open('concreteness.txt') as f:
+    for l in f.readlines():
+        fields = l.split('\t')
+        if fields[1] == 'Bigram':
+            continue # this is the first line
+        concreteness[fields[0]] = float(fields[2])
+        percent_known[fields[0]] = float(fields[6])
+
 good_words = set(accuracy.keys())
 for w in sorted(good_words):
     if accuracy[w] < 0.6:
